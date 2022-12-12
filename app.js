e2263d6c5d62169f3afff3928d6f7ad10c08f677
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-12 17:06:25 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+
+p2Button.addEventListener("click", function () {
+  alert("Hello p1");
+});
