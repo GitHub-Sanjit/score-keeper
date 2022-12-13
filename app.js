1b24958f2b5ea0ff606c9d705bf3724f2b15d420
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-13 14:38:15 +0600

    Score keeper completed

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
-const p1Button = document.querySelector("#p1Button");
-const p2Button = document.querySelector("#p2Button");
+const p1 = {
+  score: 0,
+  button: document.querySelector("#p1Button"),
+  display: document.querySelector("#p1Display"),
+};
+const p2 = {
+  score: 0,
+  button: document.querySelector("#p2Button"),
+  display: document.querySelector("#p2Display"),
+};
 
 const resetButton = document.querySelector("#reset");
-
-const p1Display = document.querySelector("#p1Display");
-const p2Display = document.querySelector("#p2Display");
 const winningScoreSelect = document.querySelector("#playto");
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener("click", function () {
+function updateScore(player, opponent) {
   if (!isGameOver) {
-    p1Score += 1;
-    if (p1Score === winningScore) {
+    player.score += 1;
+    if (player.score === winningScore) {
       isGameOver = true;
-      p1Display.classList.add("winner");
-      p2Display.classList.add("loser");
+      player.display.classList.add("has-text-success");
+      opponent.display.classList.add("has-text-danger");
+      player.button.disabled = true;
+      opponent.button.disabled = true;
     }
-    p1Display.textContent = p1Score;
+    player.display.textContent = player.score;
   }
-  if (p1Score !== winningScore) {
-  }
+}
+
+p1.button.addEventListener("click", function () {
+  updateScore(p1, p2);
 });
 
-p2Button.addEventListener("click", function () {
-  if (!isGameOver) {
-    p2Score += 1;
-    if (p2Score === winningScore) {
-      isGameOver = true;
-      p2Display.classList.add("winner");
-      p1Display.classList.add("loser");
-    }
-    p2Display.textContent = p2Score;
-  }
-  if (p2Score !== winningScore) {
-  }
+p2.button.addEventListener("click", function () {
+  updateScore(p2, p1);
 });
 
 winningScoreSelect.addEventListener("change", function () {
@@ -49,10 +46,12 @@ resetButton.addEventListener("click", reset);
 
 function reset() {
   isGameOver = false;
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.textContent = 0;
-  p2Display.textContent = 0;
-  p1Display.classList.remove("winner", "loser");
-  p2Display.classList.remove("loser", "winner");
+  p1.score = 0;
+  p2.score = 0;
+  p1.display.textContent = 0;
+  p2.display.textContent = 0;
+  p1.display.classList.remove("has-text-success", "has-text-danger");
+  p2.display.classList.remove("has-text-danger", "has-text-success");
+  p1.button.disabled = false;
+  p2.button.disabled = false;
 }
